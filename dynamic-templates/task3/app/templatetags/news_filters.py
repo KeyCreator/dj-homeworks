54fe82fc885546d927c10f8b27608a9e7662226b
Sergey Klimov <s.klimov@bk.ru>
2020-05-05 22:07:53 +0700

    Домашнее задание ко 3-ой лекции: приступил к задаче 3

--- dynamic-templates/task3/app/templatetags/news_filters.py
+++ dynamic-templates/task3/app/templatetags/news_filters.py
@@ -1,4 +1,6 @@
 from django import template
+from datetime import datetime
+import math
 
 
 register = template.Library()
@@ -7,11 +9,27 @@ register = template.Library()
 @register.filter
 def format_date(value):
     # Ваш код
+    creation_time = datetime.fromtimestamp(value)
+    current_time = datetime.utcnow()
+    difference = (current_time - creation_time).seconds
+    difference = difference/60
+
+    if difference <= 10:
+        value = 'только что'
+    elif difference < 60:
+        value = 'менее часа назад'
+    elif difference <= 24 * 60:
+        value = f'{int(difference/60)} часов назад'
+    else:
+        value = creation_time.date()
+
     return value
 
 
 # необходимо добавить фильтр для поля `score`
-
+@register.filter
+def format_score(value):
+    return
 
 @register.filter
 def format_num_comments(value):
