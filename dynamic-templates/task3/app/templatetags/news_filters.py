bf594e3bcdb386a1dfa3d400a76fde287e137511
Sergey Klimov <s.klimov@bk.ru>
2020-05-06 13:36:24 +0700

    Домашнее задание ко 3-ой лекции: осталось научиться полуучать последние слова в строке

--- dynamic-templates/task3/app/templatetags/news_filters.py
+++ dynamic-templates/task3/app/templatetags/news_filters.py
@@ -1,6 +1,6 @@
 from django import template
 from datetime import datetime
-import math
+import re
 
 
 register = template.Library()
@@ -28,13 +28,59 @@ def format_date(value):
 
 # необходимо добавить фильтр для поля `score`
 @register.filter
-def format_score(value):
-    return
+def format_score(value, default=None):
+    ''' Рейтинг меньше -5, пишет "все плохо"
+        Рейтинг от -5 до 5 – "нейтрально"
+        Рейтинг больше 5 – "хорошо"
+        Если поле score отсутствует, то рендерится дефолтное значение, которое передается в качестве параметра фильтра
+    '''
+    if not value:
+        return default
+
+    score = int(value)
+    if score < -5:
+        value = "все плохо"
+    elif score <= 5:
+        value = "нейтрально"
+    else:
+        value = "хорошо"
+
+    return value
 
 @register.filter
 def format_num_comments(value):
+    ''' Если комментариев 0, пишется "Оставьте комментарий"
+        От 0 до 50, пишем число комментариев
+        Больше 50, пишем "50+"
+    '''
     # Ваш код
+    count = int(value)
+    if not count:
+        value = "Оставьте комментарий"
+    elif count > 50:
+        value = "50+"
+
     return value
 
+@register.filter
+def format_selftext(value, count=0):
+    '''Оставляет count первых и count последних слов, между ними должно быть троеточие.
+    count задается параметром фильтра.
+    Пример c count = 5: "Hi all sorry if this ... help or advice greatly appreciated."
+    Знаки препинания остаются, обрезаются только слова.
+    '''
+    # return value
+    if not count:
+        return value
 
+    pattern = re.compile('\w+')
+    matches = pattern.findall(value)
+    print(f'Длина текста {len(matches)}')
 
+    if len(matches) > count * 3:
+        pattern = re.compile('\A(\W*\w+\W+){5}')
+        value = pattern.match(value)
+        print(value)
+        value = value.group(0) + '...'
+
+    return value
