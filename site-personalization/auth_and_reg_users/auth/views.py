a16975daa3aa48daa7d9abd6332a20b7876ebae9
Sergey Klimov <s.klimov@bk.ru>
2020-05-24 16:27:38 +0700

    Задания 1 и 2 к site-personalization

--- site-personalization/auth_and_reg_users/auth/views.py
+++ site-personalization/auth_and_reg_users/auth/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render, redirect
 from django.views.generic.edit import FormView
 from django.views.generic import TemplateView
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.http import HttpResponseRedirect
-from django.views.generic.base import View
-from django.contrib.auth import login, logout
+# from django.http import HttpResponseRedirect
+# from django.views.generic.base import View
+# from django.contrib.auth import login, logout
 
 from .models import CustomUser
 
@@ -24,9 +24,22 @@ class SignupFormView(FormView):
     form_class = UserCreationForm
     success_url = '/login/'
 
-    def form_valid(self, form):
-        form.save()
-        return super(SignupFormView, self).form_valid(form)
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
 
-    def form_invalid(self, form):
-        return super(SignupFormView, self).form_invalid(form)
+        if form.is_valid():
+            form_data = form.cleaned_data
+            user = CustomUser.objects.create_user(username=form_data['username'],
+                                                  password=form_data['password'])
+            user.save()
+
+        return redirect('/')
+
+
+    # def form_valid(self, form):
+    #     data=form.cleaned_data
+    #     form.save()
+    #     return super(SignupFormView, self).form_valid(form)
+    #
+    # def form_invalid(self, form):
+    #     return super(SignupFormView, self).form_invalid(form)
